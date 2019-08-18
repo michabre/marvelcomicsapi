9602306d8321d38d2ebc9678ba2e3a0da298c48d
Michael <michaelcbreuer@gmail.com>
2019-08-17 21:38:32 -0700

    improved finder code. removed CryptoJS.

--- js/finder.js
+++ js/finder.js
@@ -1,29 +1,29 @@
 /**
- * 
+ *
  * version 0.1
- * 
+ *
  */
 (function ($, Drupal, drupalSettings) {
 
-    "use strict";
+  "use strict";
 
-    Drupal.behaviors.marvelcomicsapiFinder = {
-      attach: function (context, settings) {
-        $(window, context).once('marvelcomicsapiFinder').each(function () {  
-            // add code here to avoid being triggered every AJAX request
-            var requestUrl = window.location.protocol + '//' + window.location.hostname + drupalSettings.path.baseUrl + 'marvelcomicsapi/json';
-            var detailsSection = $('#marvelcomicsapi-finder .details');
+  Drupal.behaviors.marvelcomicsapiFinder = {
+    attach: function (context, settings) {
+      $(window, context).once('marvelcomicsapiFinder').each(function () {
+        // add code here to avoid being triggered every AJAX request
+        const site_url = window.location.protocol + "//" + window.location.host;
+        const requestUrl = site_url + '/marvelcomicsapi/json?type=characters&character=';
+        const detailsSection = $('#marvelcomicsapi-finder .details');
+        const input = '#find-marvelcomics-character input';
 
-            //
-            $('#find-marvelcomics-character button').on('click', function(event) {
-                event.preventDefault();
-                var name = $('#find-marvelcomics-character input').val();
-                var request = requestUrl + '?character=' + name;
-
-                console.log(request);
-                Drupal.theme('fetchData', detailsSection, request);
-            });
+        //
+        $('#find-marvelcomics-character button').on('click', function (event) {
+          event.preventDefault();
+          const name = $('#find-marvelcomics-character input').val();
+          const request = requestUrl + name;
+          Drupal.theme('getCharacterData', detailsSection, request, input);
         });
-      },
-    };
-  })(jQuery, Drupal, drupalSettings);
+      });
+    },
+  };
+})(jQuery, Drupal, drupalSettings);
